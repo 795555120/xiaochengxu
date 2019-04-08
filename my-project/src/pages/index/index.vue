f7e3a1a6a35f1eba3fbcd5cc1ff115d18eb39435
student <student@hubu.edu.cn>
2019-04-08 17:23:25 +0800

    4.8

--- my-project/src/pages/index/index.vue
+++ my-project/src/pages/index/index.vue
@@ -1,15 +1,52 @@
 <template>
   <div >
+    <i-notice-bar icon="systemprompt" loop>
+    漫文寨测试版1.0上线了，欢迎大家！
+    </i-notice-bar>
     <i-grid>
-    <i-grid-item>
+      <i-grid-item>
         <i-grid-icon>
-            <image src="../images/icon-vip.png" />
+            <image src="../../static/images/shu.png" />
         </i-grid-icon>
-        <i-grid-label>Grid</i-grid-label>
-    </i-grid-item>
-    <i-grid-item>Grid</i-grid-item>
-    <i-grid-item>Grid</i-grid-item>
-</i-grid>
+        <i-grid-label>漫画</i-grid-label>
+      </i-grid-item>
+      <i-grid-item>
+        <i-grid-icon>
+            <image src="../../static/images/shu2.png" />
+        </i-grid-icon>
+        <i-grid-label>网文</i-grid-label>
+      </i-grid-item>
+      <i-grid-item>
+        <i-grid-icon>
+            <image src="../../static/images/shu3.png" />
+        </i-grid-icon>
+        <i-grid-label>同人</i-grid-label>
+      </i-grid-item>
+    </i-grid>
+    <i-panel title="标题">
+      <view style="padding: 15px;"> 
+        <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+            <view slot="content">内容不错</view>
+            <view slot="footer">尾部内容</view>
+        </i-card>   
+      </view>
+    </i-panel>
+        <i-panel title="标题">
+      <view style="padding: 15px;"> 
+        <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+            <view slot="content">内容不错</view>
+            <view slot="footer">尾部内容</view>
+        </i-card>   
+      </view>
+    </i-panel>
+        <i-panel title="标题">
+      <view style="padding: 15px;"> 
+        <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+            <view slot="content">内容不错</view>
+            <view slot="footer">尾部内容</view>
+        </i-card>   
+      </view>
+    </i-panel>
   </div>
 </template>
 
