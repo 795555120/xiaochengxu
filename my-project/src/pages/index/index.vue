37f991de649c4a094ca006ba27553f03911ddcc4
student <student@hubu.edu.cn>
2019-05-20 18:06:16 +0800

    5.20

--- my-project/src/pages/index/index.vue
+++ my-project/src/pages/index/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div >
     <i-notice-bar icon="systemprompt" loop>
-    漫文寨测试版1.0上线了，欢迎大家！
+    漫文寨测试版1.0上线了，谢谢大家的支持！
     </i-notice-bar>
 
     <i-grid>
@@ -24,7 +24,8 @@
         <i-grid-label>同人</i-grid-label>
       </i-grid-item>
     </i-grid>
-    <i-panel title="热榜">
+
+    <i-panel :title="title_name">
       <view style="padding: 0px;"> 
         <i-card title="鹿瑶知马力"  thumb ="http://imgsrc.baidu.com/forum/pic/item/0135c3ed8a136327d67207349f8fa0ec09fac702.jpg">
             <view slot="content">不知道为什么</view>
@@ -54,22 +55,10 @@
 </template>
 
 <script>
-import { formatTime } from '@/utils/index'
-import card from '@/components/card'
-
 export default {
-  components: {
-    card
-  },
-
   data () {
     return {
-      logs: [],
-      imgUrls: [
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/newsPicture/05558951-de60-49fb-b674-dd906c8897a6',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/coursePicture/0fbcfdf7-0040-4692-8f84-78bb21f3395d',
-        'http://mss.sankuai.com/v1/mss_51a7233366a4427fa6132a6ce72dbe54/management-school-picture/7683b32e-4e44-4b2f-9c03-c21f34320870'
-      ]
+      title_name:"热榜"
     }
   },
 
